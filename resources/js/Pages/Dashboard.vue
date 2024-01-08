cb2377fff775a035407f7d3e91c46657a568aad3
juborajnaofel <juborajnaofel@gmail.com>
2024-01-08 23:29:27 +0600

    chat room initial UI

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,10 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+const currentUser = ref(3);
+
 </script>
 
 <template>
@@ -8,13 +12,55 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Chat Room</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="flex flex-row p-3">
+                        <div class="w-1/4 bg-slate-200 p-2">
+                            Chat Rooms
+                            <div class="h-[600px] overflow-auto">
+                                <span v-for="item in [1,2,3,4,5,6,7,8, 9, 10,11]">
+                                    <div class="flex flex-row bg-slate-300 rounded-md p-3 m-3">
+                                        {{ item }}
+                                    </div>
+                                </span>
+                            </div>
+
+                        </div>
+                        <div class="w-3/4 h-[600px] p-2">
+                            <div class="flex flex-col">
+                                <div class="h-[550px] overflow-auto">
+                                    <span v-for="item in [{message:1, user_id: 1} ,{message:2, user_id:1}, {message:3, user_id:3}]">
+                                        <div class="m-3" v-if="item.user_id !== currentUser">
+                                            <div class="flex flex-row">
+                                                <div class="w-3/4 bg-slate-200 rounded-md p-3">
+                                                    {{ item.message }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="m-3" v-else>
+                                            <div class="flex flex-row justify-end ">
+                                                <div class="w-3/4 bg-slate-200 rounded-md p-3">
+                                                    {{ item.message }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </div>
+                                <div class="h-[50px]">
+                                    <form action="">
+                                        <div class="flex flex-row">
+                                            <input type="text" class="w-5/6 rounded-md"/>
+                                            <button class="w-1/6 bg-slate-200 rounded-md">Send</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
